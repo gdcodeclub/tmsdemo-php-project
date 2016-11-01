84b5aca027b06e193e10e00ef3ecb36c57ebb789
richardgfong <richardgfong@gmail.com>
2016-11-01 08:56:48 -0500

    update to ProcessConroller to tray and get request data into confirm controller

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -19,13 +19,13 @@ class ProcessController extends Controller
     public function process(Request $request)
     {
         $deliveryType = $request->input('emailSms');
-        if($deliveryType == 'emailSend'){
 
+        if ($deliveryType == 'emailSend') {
         	$subject = $request->input('subject');
         	$message = $request->input('message');
 
-            //split the recipient email address by comma delimiter
-            //allows for multiple recipients
+            // split the recipient email address by comma delimiter
+            // allows for multiple recipients
             $recipients = explode(",", $request->input('recipient'));
             $deliveryType = $request->input('emailSms');
 
@@ -39,13 +39,13 @@ class ProcessController extends Controller
     		  'open_tracking_enabled' => true,
     		));
 
-            //foreach var in recipients - see above - run the email recipients->build
-            foreach ($recipients as $request => $recipient) {   
-    		  $email->recipients->build(array('email' => $recipient));
-            }    
+            // foreach var in recipients - see above - run the email recipients->build
+            foreach ($recipients as $recipient) {       		    
+                $email->recipients->build(array('email' => $recipient));
+            }   
+
     		$email->post();
-        }
-        else{
+        } else {
             $message = $request->input('message');
             $recipients = explode(",", $request->input('recipient'));
             $client = new Client($_ENV["TMS_KEY"],$_ENV["TMS_ENDPOINT"]);
@@ -54,24 +54,26 @@ class ProcessController extends Controller
               'body' => $message,
             ));
 
-            //foreach var in recipients - see above - run the email recipients->build
-            foreach ($recipients as $request => $recipient) {   
+            // foreach var in recipients - see above - run the email recipients->build
+            foreach ($recipients as $recipient) {   
                 $sms->recipients->build(array('phone' => $recipient));
-            }    
+            }
+
             $sms->post();
         }    
 
-        //var_dump($email);
-        //var_dump($request->all());
+        // $request->session()->flash('data', $request);
 
-        // return "<p>Subject: <br />Message: $message<br />Recipient: $recipient</p>$deliveryType</br>";
         return redirect()->route('confirm');
 
     }
 
-    public function confirm(Request $request){
-        var_dump($request->all());
-        return "Here we are in the confirm!";
+    public function confirm(Request $request) 
+    {
+        // var_dump($request->session());
+        // var_dump($request->session()->get('data'));
+        
+        return "Success! Your messages is being delivered right now!";
     }
 
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,13 +59,11 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
