a62d9e146405bdb964ca57515c3b01549af51d8e
Richard Fong <rfong@rfong-mbp.local>
2017-05-10 09:02:17 -0500

    This is the migration to create the message table.

---
+++ database/migrations/2017_05_10_132204_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('body')->varchar(1000);
+            $table->string('subject')->varchar(256);
+            $table->string('message_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
